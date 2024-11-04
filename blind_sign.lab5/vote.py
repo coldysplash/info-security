eae2236a692f5a97bcd71b683c8fa778ce5156ef
Ivan <ivanreshetnikov235@gmail.com>
2024-11-04 13:20:47 +0700

    fix lab5

--- blind_sign.lab5/vote.py
+++ blind_sign.lab5/vote.py
@@ -18,36 +18,41 @@ class Server:
     def sign_vote(self, h_encr):
         return pow(h_encr, self.c, self.N)
 
-    def check_sign(self, sign, h_encry):
-        if sign != h_encry:
+    def check_sign(self, hash_vote, sign):
+        if hash_vote != pow(sign, self.d, self.N):
             return -1
         else:
             return 0
 
 
+def md5_hash(string: str):
+    hash_md5 = hashlib.md5(string.encode()).digest()
+    res_hash = 0
+    for i in range(16):
+        res_hash += hash_md5[i]
+    return res_hash
+
+
 def client(res_vote: str, server: Server):
     d, N = server.get_public_params()
     rnd = random.randint(100, 10000)
-    v = 0
-    for ch in res_vote:
-        v += ord(ch)
-    n = rnd + v
+    if res_vote.lower() == "yes":
+        v = 1
+    else:
+        v = 0
+    n = str(rnd) + str(v)
     r = generate_prime_number(1, N - 1)
     while gcd(r, N) != 1:
         r += 1
 
-    hash_n = hashlib.sha256(str(n).encode()).digest()
-
-    h_encryption = 0
-    for i in range(16):
-        h_encryption += (hash_n[i] * r**d) % N
-
+    n_hash = md5_hash(n)
+    h_encryption = (n_hash * r**d) % N
     s_encry = server.sign_vote(h_encryption)
 
     r_inv = pow(r, -1, N)
     sign = (s_encry * r_inv) % N
 
-    return server.check_sign(sign, h_encryption)
+    return server.check_sign(n_hash, sign)
 
 
 if __name__ == "__main__":
